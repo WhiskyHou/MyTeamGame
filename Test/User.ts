db41e1fc51b2cc1eed07ffe0ae306113cec6ac85
DRiV <505225308@qq.com>
2018-07-03 10:30:15 +0800

    add user and Equips

---
+++ Test/User.ts
@@ -0,0 +1,80 @@
+class User{
+
+    public name;
+    private _originDamage=20;
+    private _originHealth=100;
+
+    private equipments:Equipment[];
+    private _attack=0;
+    private _health=0;
+    private _criticalPer=0;
+
+    private _suitDefensePer=0;
+    private _suitDamagePer=0;
+    private _suitCriticalPer=0;
+
+    public changeEquipments(){
+       this.initProperty();
+        for(var i=0;i<this.equipments.length;i++){
+            this._attack+= this.equipments[i].attack;
+            this._health+=this.equipments[i].health;
+            this._criticalPer+=this.equipments[i].criticalPer;
+        }
+        this.checkSuit();
+    }
+
+    //TODO:套装属性检测
+    private checkSuit(){
+        let suitNum:number[];
+        for(var i=0;i<this.equipments.length;i++){
+
+        }
+    }
+
+    public dressEquip(equip:Equipment){
+        this.equipments[equip.posID]=equip;
+        this.changeEquipments();
+    }
+
+    private initProperty(){
+        this._attack=this._originDamage;
+        this._health=this._originHealth;
+        this._criticalPer=0;
+
+        this._suitDefensePer=0;
+        this._suitDamagePer=0;
+        this._suitCriticalPer=0;
+    }
+
+    public dealDamage():number{
+       let ran=  Math.random() * 100;
+        if(ran<=this._criticalPer){
+            return this.normalDamage()*2;
+        }
+        return this.normalDamage();
+    }
+
+    private normalDamage():number{
+        return this._attack*(1+this._suitDamagePer)*this.damageFlow();
+    }
+
+    private damageFlow():number{
+        let ran=Math.random();
+        let val=1;
+        if(ran<=50){
+            val=-1;
+        }
+        return (1+ 2 * val * Math.random()/1000 ); //伤害浮动幅度为0.8~1.2
+    }
+
+    public beDamaged(dmg:number){
+        this._health-= dmg*(1-this._suitDefensePer);
+        if(this._health<=0){
+            this.die();
+        }
+    }
+
+    private die(){
+
+    }
+}
