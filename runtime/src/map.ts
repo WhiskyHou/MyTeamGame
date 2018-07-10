6056132842c3ebe4681e5a0b0d61eb016f966aea
whiskyHou_Rog <ghostgaz@126.com>
2018-07-10 14:03:43 +0800

    fix bug

--- runtime/src/map.ts
+++ runtime/src/map.ts
@@ -75,7 +75,7 @@ class GameMap extends DisplayObjectContainer {
                 }
             }
 
-            const mapWalkable = obj[0].walkable as number[][]
+            const mapWalkable = obj.map[0].walkable as number[][]
             for (let i = 0; i < mapWalkable.length; i++) {
                 const row = mapWalkable[i]
                 for (let j = 0; j < row.length; j++) {
@@ -144,26 +144,6 @@ class GameMap extends DisplayObjectContainer {
                 }
             }
 
-            // const mapMonster = obj.map[0].monster as number[][]
-            // for (let i = 0; i < mapMonster.length; i++) {
-            //     const row = mapMonster[i];
-            //     for (let j = 0; j < row.length; j++) {
-            //         const id = row[j]
-            //         if (id) {
-            //             const monsterView = new Bitmap(TILE_SIZE * i, TILE_SIZE * j, captain);
-            //             const monsterItem = new Monster(1, '队长', 1200, 100);
-            //             monsterItem.name = '队长';
-            //             monsterItem.view = monsterView;
-            //             // monsterItem.hp = 120;
-            //             monsterItem.x = i;
-            //             monsterItem.y = j;
-            //             const key = i + '_' + j;
-            //             this.monsterConfig[key] = monsterItem;
-            //             this.roleContainer.addChild(monsterView);
-            //         }
-            //     }
-            // }
-
             const mapMonster = obj.map[0].monster as number[][];
             for (let i = 0; i < mapMonster.length; i++) {
                 const row = mapMonster[i];
@@ -178,10 +158,10 @@ class GameMap extends DisplayObjectContainer {
                             if (monster.id == id) {
                                 const monsterView = monster.view;
                                 // const npcHead = npc.head;
-                                monsterView.x = TILE_SIZE * i;
-                                monsterView.y = TILE_SIZE * j;
-                                monster.x = i
-                                monster.y = j
+                                monsterView.x = TILE_SIZE * j;
+                                monsterView.y = TILE_SIZE * i;
+                                monster.x = j
+                                monster.y = i
                                 const key = i + '_' + j;
                                 this.monsterConfig[key] = monster;
                                 this.roleContainer.addChild(monsterView);
